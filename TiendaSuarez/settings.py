69c6f42c44da92a5b8734e572837dbaf0b24fd41
Alejandro De La Cruz <97196517+DevLaCruz@users.noreply.github.com>
2023-09-02 21:44:15 -0500

    agregando link de host

--- TiendaSuarez/settings.py
+++ TiendaSuarez/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['suarez-env.eba-erdus35d.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
