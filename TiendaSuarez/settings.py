88f87ababe3eaf4759f4334bde68a441c918335e
Alejandro De La Cruz <97196517+DevLaCruz@users.noreply.github.com>
2023-09-07 09:48:43 -0500

    ya

--- TiendaSuarez/settings.py
+++ TiendaSuarez/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['ecommerce-env.eba-d3gtfxms.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ["devlacruz-env.eba-swfxfhec.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
